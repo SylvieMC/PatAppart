4c027ddcc9b13d5d71e18c2fc696c9c0a6e975ac
Sylvie M.Cassim <sylvie.cassim@gmail.com>
2020-03-17 22:17:51 +0100

    add routes animal

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,12 +3,7 @@ const RouteManager = function() { };
 RouteManager.attach = function(app) {
   app.use('/logements' , require('./logement'));
   app.use('/utilisateurs' , require('./utilisateur'));
-  /*app.use('/menu' , require('./menu'));
-  app.use('/ingredient', require('./ingredient'));
-  app.use('/product', require('./product'));
-  app.use('/promotion', require('./promotion'));
-  app.use('/user', require('./user'));
-  app.use('/order', require('./order'));*/
+  app.use('/animals' , require('./animal'));
 };
 
 module.exports = RouteManager;
