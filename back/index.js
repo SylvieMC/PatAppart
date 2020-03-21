e68633fa58d969ad23cc7bb722def437f98390ae
Sylvie M.Cassim <sylvie.cassim@gmail.com>
2020-03-21 19:41:40 +0100

    remove s

--- back/index.js
+++ back/index.js
@@ -20,7 +20,7 @@ var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
-    } else {s
+    } else {
       callback(new Error('Not allowed by CORS'))
     }
   }
