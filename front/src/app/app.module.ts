d404e383ec3656249129ba5f1c47dc4969b1fa15
Sylvie M.Cassim <sylvie.cassim@gmail.com>
2020-03-26 01:07:45 +0100

    create connexion - in progress

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { LocationsComponent } from './locations/locations.component';
 import { InscriptionComponent } from './inscription/inscription.component';
@@ -18,8 +17,6 @@ import {MatInputModule} from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ConnexionComponent } from './connexion/connexion.component';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +24,7 @@ import { ConnexionComponent } from './connexion/connexion.component';
     HeaderComponent,
     LocationsComponent,
     InscriptionComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
